3d1d6d9fad027f034d09d926fdb7468d53d17c3b
liwei <liwei821172349@163.com>
2018-04-15 15:59:24 +0800

    Merge branch 'master' of https://github.com/qinjian0306/community
    # Conflicts:
    #	src/main/java/org/chinamyheart/community/service/UserService.java

--- src/main/java/org/chinamyheart/community/service/UserService.java
+++ src/main/java/org/chinamyheart/community/service/UserService.java
@@ -1,8 +1,18 @@
 package org.chinamyheart.community.service;
 
+import org.chinamyheart.community.common.utils.Utils;
+import org.chinamyheart.community.mapper.UserMapper;
+import org.chinamyheart.community.model.Doctor;
 import org.chinamyheart.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
+
+	User selectByUsername(String username);
+
+	String insertByUser(User User);
+
 	User getUser(String token);
 }
