b7418ffd30ec635b1f85786d70885284128c0f14
liwei <liwei821172349@163.com>
2018-04-15 15:57:55 +0800

    Merge branch 'master' of https://github.com/qinjian0306/community
    # Conflicts:
    #	src/main/java/org/chinamyheart/community/service/UserService.java

--- src/main/java/org/chinamyheart/community/service/impl/UserServiceImpl.java
+++ src/main/java/org/chinamyheart/community/service/impl/UserServiceImpl.java
@@ -1,13 +1,29 @@
 package org.chinamyheart.community.service.impl;
 
+import org.chinamyheart.community.mapper.UserMapper;
 import org.chinamyheart.community.model.User;
 import org.chinamyheart.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
+	@Autowired
+	private UserMapper userMapper;
 	@Override
 	public User getUser(String token) {
 		return null;
+	}
+
+	@Override
+	public User selectByUsername(String username) {
+		User user = userMapper.selectByUsername(username);
+		return user;
+	}
+
+	@Override
+	public String insertByUser(User user) {
+		userMapper.insertByUser(user);
+		return "success";
 	}
 }
